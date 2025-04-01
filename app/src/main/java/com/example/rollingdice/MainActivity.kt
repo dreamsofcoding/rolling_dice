dcd965f040e0c84ff8b67a75e6cdb1582dadfb9a
dreamsofcoding <codingdreams@pm.me>
2025-04-01 16:35:52 +0100

    adding in the dice image functionality for each roll

--- app/src/main/java/com/example/rollingdice/MainActivity.kt
+++ app/src/main/java/com/example/rollingdice/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -20,7 +22,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.rollingdice.ui.theme.RollingDiceTheme
 import kotlin.random.Random
@@ -50,13 +54,34 @@ fun RollingScreen(padding: PaddingValues) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        DiceRollValueLabel(
-            value = diceValue.toString(),
-            modifier = Modifier.padding(padding)
-        )
+        DiceImage(diceValue)
+
+        Spacer(Modifier.height(30.dp))
+
+        DiceRollValueLabel(diceValue.toString())
+
+        Spacer(Modifier.height(30.dp))
 
         RollButton(onRoll = { diceValue = rollDice() })
     }
+}
+
+@Composable
+fun DiceImage(diceValue: Int) {
+    val imageResource = when(diceValue) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Image(
+        painter = painterResource(id = imageResource),
+        contentDescription = diceValue.toString()
+
+    )
 }
 
 @Composable
@@ -71,7 +96,7 @@ fun RollButton(onRoll: () -> Unit) {
 }
 
 fun rollDice(): Int {
-    return Random.nextInt(1, 6)
+    return Random.nextInt(1, 7)
 }
 
 @Composable
